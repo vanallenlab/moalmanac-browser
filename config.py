fa1e2ac7661f5f612e3bcc1d479d1c78b411db70
Eric Kofman <erickofman@erics-mbp.dfci.harvard.edu>
2017-11-17 14:20:23 -0500

    Update config to reflect new db file name

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 from secret_key import GCP_SECRET_KEY
 
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///target.sqlite3'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///salsa110.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = GCP_SECRET_KEY
 BOOTSTRAP_SERVE_LOCAL = True
