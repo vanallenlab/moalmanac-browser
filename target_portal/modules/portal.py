1a8b298e1520df50a54df6cc0fa196fec9aef8bf
Eric Kofman <ericrkofman@gmail.com>
2017-12-20 16:49:56 -0500

    Add row page initial work

--- target_portal/modules/portal.py
+++ target_portal/modules/portal.py
@@ -5,12 +5,15 @@ from .models import Alteration, Assertion, Source, AssertionToAlteration, Assert
 
 portal = Blueprint('portal', __name__)
 
+
 def _filter_row_column(query_results, column):
     return [getattr(row, column) for row in query_results if getattr(row, column) not in [None, '']]
+
 
 def _query_distinct_column(model, column):
     query = db.session.query(getattr(model, column).distinct().label(column))
     return _filter_row_column(query, column)
+
 
 @portal.route('/')
 def index():
@@ -27,19 +30,44 @@ def index():
                            num_genes=num_genes,
                            num_assertions=num_assertions,
                            typeahead_genes=typeahead_genes,
-                           diseases=diseases,
-                           pred_impls=pred_impls,
-                           therapy_names=therapy_names)
+                           diseases=[d for d in sorted(diseases) if not d == 'Oncotree Term'],
+                           pred_impls=[i for i in sorted(pred_impls) if not i == 'Predictive implication'],
+                           therapy_names=[t for t in sorted(therapy_names) if not t == 'Therapy name']
+                           )
+
 
 @portal.route('/about')
 def about():
     return render_template('portal_about.html',
                            nav_current_page='about')
 
+
 @portal.route('/suggest')
 def suggest():
     return render_template('portal_suggest.html',
                            nav_current_page='suggest')
+
+
+@portal.route('/add')
+def add():
+    typeahead_genes = _query_distinct_column(Alteration, 'gene_name')
+    diseases = _query_distinct_column(Assertion, 'disease')
+    pred_impls = _query_distinct_column(Assertion, 'predictive_implication')
+    therapy_names = _query_distinct_column(Assertion, 'therapy_name')
+
+    num_genes = db.session.query(Alteration.gene_name).distinct().count()
+    num_assertions = db.session.query(Assertion).count()
+
+    return render_template('portal_add.html',
+                           nav_current_page='add',
+                           num_genes=num_genes,
+                           num_assertions=num_assertions,
+                           typeahead_genes=typeahead_genes,
+                           diseases=[d for d in sorted(diseases) if not d == 'Oncotree Term'],
+                           pred_impls=[i for i in sorted(pred_impls) if not i == 'Predictive implication'],
+                           therapy_names=[t for t in sorted(therapy_names) if not t == 'Therapy name']
+                           )
+
 
 @portal.route('/search')
 def search():
@@ -93,6 +121,7 @@ def search():
                            typeahead_genes=typeahead_genes,
                            pred_impl_orders=pred_impl_orders,
                            rows=rows)
+
 
 @portal.route('/assertion/<int:assertion_id>')
 def assertion(assertion_id):
