e84249d3601b659f275f3407061c4a5dc6b85265
Nathanael Moore <nate.moore@mooreproject.com>
2019-04-03 17:16:17 -0400

    Implement caching of Select2 data.
    To reduce AJAX calls to the server, the results of each category's AJAX calls are stored in the browser's local storage, where they are drawn from on subsequent page views. A cache expiry value is set alongside the storage data, which is configurable in portal-select2.js; it is currently set to 5 minutes.

--- static/js/portal-select2.js
+++ static/js/portal-select2.js
@@ -1,3 +1,13 @@
+const cache_expiry = 1000*60*5; // cache expiration time in milliseconds
+
+$(document).ready(function() {
+    $('.search').select2({
+        theme: 'bootstrap',
+        multiple: true,
+        containerCssClass: 'select2-font',
+    });
+});
+
 function escapeRegexStr(str) {
     return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
@@ -15,7 +25,7 @@ function tokenInSearchParams(token) {
 
 // load data objects from remote JSON
 function getJSONSearchSet(url, category, callback) {
-    $.getJSON(url, function(data) {
+    return $.getJSON(url, function(data) {
         let search_set = Object;
         search_set = [];
         $.each(data, function(index, value) {
@@ -23,7 +33,6 @@ function getJSONSearchSet(url, category, callback) {
             new_datum.id = '"' + value + '"';
             new_datum.text = value;
             new_datum.category = category;
-            new_datum.selected = tokenInSearchParams(value);
 
             search_set.push(new_datum);
         });
@@ -32,28 +41,46 @@ function getJSONSearchSet(url, category, callback) {
     });
 }
 
-let search_space = [];
+let search_space = localStorage.getItem('search_space');
+let search_space_timestamp = parseInt(localStorage.getItem('search_space_timestamp'));
+const cache_expiry_threshold = Date.now() - cache_expiry;
 
-let features = Object();
-features.text = 'Features';
-getJSONSearchSet('api/features', 'feature', function(data) {features.children = data;});
-search_space.push(features);
+if (search_space && search_space_timestamp > cache_expiry_threshold) {
+    search_space = JSON.parse(search_space);
+    load_select2_data();
+} else {
+    search_space = [];
 
-let diseases = Object();
-diseases.text = 'Diseases';
-getJSONSearchSet('api/diseases', 'disease', function(data) {diseases.children = data;});
-search_space.push(diseases);
+    let features = Object(), diseases = Object(), therapies = Object(), preds = Object();
+    features.text = 'Features';
+    diseases.text = 'Diseases';
+    therapies.text = 'Therapies';
+    preds.text = 'Predictive Implications';
 
-let therapies = Object();
-therapies.text = 'Therapies';
-getJSONSearchSet('api/therapies', 'therapy', function(data) {therapies.children = data;});
-search_space.push(therapies);
+    $.when(
+            getJSONSearchSet('api/features', 'feature', function (data) {
+                features.children = data;
+            }),
+            getJSONSearchSet('api/diseases', 'disease', function (data) {
+                diseases.children = data;
+            }),
+            getJSONSearchSet('api/therapies', 'therapy', function (data) {
+                therapies.children = data;
+            }),
+            getJSONSearchSet('api/predictive_implications', 'pred', function (data) {
+                preds.children = data;
+            })
+    ).then(function() {
+        search_space.push(features);
+        search_space.push(diseases);
+        search_space.push(therapies);
+        search_space.push(preds);
 
-let preds = Object();
-preds.text = 'Predictive Implications';
-getJSONSearchSet('api/predictive_implications', 'pred',
-    function(data) {preds.children = data;});
-search_space.push(preds);
+        localStorage.setItem('search_space', JSON.stringify(search_space));
+        localStorage.setItem('search_space_timestamp', Date.now().toString());
+        load_select2_data();
+    });
+}
 
 function addCategoryClass(data, container) {
     if (data.category) {
@@ -63,15 +90,17 @@ function addCategoryClass(data, container) {
     return data.text;
 }
 
-$('.search').select2({
-    theme: 'bootstrap',
-    multiple: true,
-    containerCssClass: 'select2-font',
-});
-
-$(document).ajaxStop(function() {
-    $('.search').select2({
-        'data': search_space,
-        'templateSelection': addCategoryClass
+function load_select2_data() {
+    $.each(search_space, function(index, category) {
+        $.each(category.children, function(index, child) {
+            child.selected = tokenInSearchParams(child.text);
+        });
     });
-});
+
+    $(document).ready(function() {
+        $('.search').select2({
+            'data': search_space,
+            'templateSelection': addCategoryClass
+        });
+    });
+}
