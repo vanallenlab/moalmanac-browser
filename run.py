840b28b403406b843a591d0b65900122e79b1020
Brendan Reardon <9423424+brendanreardon@users.noreply.github.com>
2025-05-01 21:56:04 -0400

    Updated run.py and gitignore

--- run.py
+++ run.py
@@ -1,14 +1,32 @@
-from almanac_browser import create_app
-import os
+import argparse
 
-app = create_app(__name__)
+from app import create_app
+
+app = create_app()
+
 
 if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+        prog='Molecular Oncology Almanac Browser',
+        description='Web browser the Molecular Oncology Almanac database'
+    )
+    arg_parser.add_argument(
+        '-m', '--mode',
+        choices=['development', 'production'],
+        help='Run the browser in development or production mode',
+        required=True
+    )
+    args = arg_parser.parse_args()
 
-	# for debugging
-	#if os.environ['FLASK_ENV'] == 'development':
-	#app.run('localhost', 8080, debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
+    if args.mode == 'development':
+        host = 'localhost'
+        port = 4000
+        debug = True
+    elif args.mode == 'production':
+        host = '0.0.0.0'
+        port = 5000
+        debug = False
+    else:
+        raise ValueError(f'Invalid mode: {args.mode}')
 
-	# for hosting
-	#else:
-	app.run(host='0.0.0.0', debug=False)
+    app.run(host=host, port=port, debug=debug)
