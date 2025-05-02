69d03b874db898274a34436dbc08ae35fdf3d8d5
Brendan Reardon <9423424+brendanreardon@users.noreply.github.com>
2025-05-01 22:07:42 -0400

    Updates to config and run

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from app import create_app
 
@@ -15,6 +16,16 @@ if __name__ == '__main__':
         choices=['development', 'production'],
         help='Run the browser in development or production mode',
         required=True
+    )
+    arg_parser.add_argument(
+        '-a', '--api',
+        choices=['http://localhost:8000', 'https://api.moalmanac.org'],
+        default='https://api.moalmanac.org',
+        help='URL for the MOAlmanac API'
+    )
+    arg_parser.add_argument(
+        '-c', '--config',
+        default=os.path.join('config', 'default.ini')
     )
     args = arg_parser.parse_args()
 
