bdae2ed2d4958f72b5a367206749d6a9066090e0
Brendan Reardon <9423424+brendanreardon@users.noreply.github.com>
2025-05-22 22:00:08 -0400

    Indications page up

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,143 +1,63 @@
+function addFilter({ filterEl, attributeName, filterKey }) {
+  $.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
+    const selectedValue = filterEl.value;
+    const row = settings.aoData[dataIndex].nTr;
+    const rowValue = row.getAttribute(attributeName);
+
+    return !selectedValue || rowValue === selectedValue;
+  });
+
+  filterEl.addEventListener('change', () => {
+    // Force all tables to redraw when a shared filter changes
+    $('.dataTable').DataTable().draw();
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  // DOCUMENTS TABLE SETUP
-  const documentsTableEl = document.querySelector('#documents-table-result');
   const organizationFilter = document.getElementById('organizationFilter');
-  if (documentsTableEl && organizationFilter) {
-    // Custom filter function
-    $.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
-      const selectedOrganization = organizationFilter.value;
-      const row = settings.aoData[dataIndex].nTr;
-      const rowOrganization = row.getAttribute('data-organization');
-
-      return !selectedOrganization || rowOrganization === selectedOrganization;
-    });
-    const documentsTable = new DataTable(documentsTableEl, {
-      autoWidth: false,
-      classes: {
-        table: 'table table-striped'
-      },
-      layout: {
-        topStart: 'search',
-        topEnd: 'pageLength',
-        bottomStart: 'info',
-        bottomEnd: 'paging'
-      },
-      pageLength: 10,
-      responsive: true
-    });
-    organizationFilter.addEventListener('change', () => documentsTable.draw());
-  }
-
-  // BIOMARKERS TABLE SETUP
-  const biomarkersTableEl = document.querySelector('#biomarkers-table-result');
-  const biomarkerTypeFilter = document.getElementById('biomarkerTypeFilter');
-  if (biomarkersTableEl && biomarkerTypeFilter) {
-    $.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
-      const selectedBiomarkerType = biomarkerTypeFilter.value;
-      const row = settings.aoData[dataIndex].nTr;
-      const rowBiomarkerType = row.getAttribute('data-biomarkerType');
-
-      return !selectedBiomarkerType || rowBiomarkerType === selectedBiomarkerType;
-    });
-    const biomarkersTable = new DataTable(biomarkersTableEl, {
-      autoWidth: false,
-      classes: {
-        table: 'table table-striped'
-      },
-      layout: {
-        topStart: 'search',
-        topEnd: 'pageLength',
-        bottomStart: 'info',
-        bottomEnd: 'paging'
-      },
-      pageLength: 10,
-      responsive: true
-    });
-    biomarkerTypeFilter.addEventListener('change',  () => biomarkersTable.draw());
-  }
-
-  // DISEASES TABLE SETUP
-  const diseasesTableEl = document.querySelector('#diseases-table-result');
-  if (diseasesTableEl) {
-    new DataTable(diseasesTableEl, {
-      autoWidth: false,
-      classes: {
-        table: 'table table-striped'
-      },
-      layout: {
-        topStart: 'search',
-        topEnd: 'pageLength',
-        bottomStart: 'info',
-        bottomEnd: 'paging'
-      },
-      pageLength: 10,
-      responsive: true
+  if (organizationFilter) {
+    addFilter({
+      filterEl: organizationFilter,
+      attributeName: 'data-organization',
+      filterKey: 'organization'
     });
   }
 
-  // GENES TABLE SETUP
-  const genesTableEl = document.querySelector('#genes-table-result');
-  if (genesTableEl) {
-    new DataTable(genesTableEl, {
-      autoWidth: false,
-      classes: {
-        table: 'table table-striped'
-      },
-      layout: {
-        topStart: 'search',
-        topEnd: 'pageLength',
-        bottomStart: 'info',
-        bottomEnd: 'paging'
-      },
-      pageLength: 10,
-      responsive: true
-    });
-  }
-
-  // PROPOSITIONS - THERAPEUTIC RESPONSE TABLE SET UP
-  const propositionsTherapeuticResponseTableEl = document.querySelector('#propositions-therapeutic-response-table-result');
-  if (propositionsTherapeuticResponseTableEl) {
-    new DataTable(propositionsTherapeuticResponseTableEl, {
-      autoWidth: false,
-      classes: {
-        table: 'table table-striped'
-      },
-      layout: {
-        topStart: 'search',
-        topEnd: 'pageLength',
-        bottomStart: 'info',
-        bottomEnd: 'paging'
-      },
-      pageLength: 10,
-      responsive: true
-    });
-  }
-
-  // THERAPIES TABLE SETUP
-  const therapiesTableEl = document.querySelector('#therapies-table-result');
   const therapyTypeFilter = document.getElementById('therapyTypeFilter');
-  if (therapiesTableEl && therapyTypeFilter) {
-    $.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
-      const selectedTherapyType = therapyTypeFilter.value;
-      const row = settings.aoData[dataIndex].nTr;
-      const rowTherapyType = row.getAttribute('data-therapyType');
-
-      return !selectedTherapyType || rowTherapyType === selectedTherapyType;
+  if (therapyTypeFilter) {
+    addFilter({
+      filterEl: therapyTypeFilter,
+      attributeName: 'data-therapyType',
+      filterKey: 'therapyType'
     });
-    const therapiesTable = new DataTable(therapiesTableEl, {
-      autoWidth: false,
-      classes: {
-        table: 'table table-striped'
-      },
-      layout: {
-        topStart: 'search',
-        topEnd: 'pageLength',
-        bottomStart: 'info',
-        bottomEnd: 'paging'
-      },
-      pageLength: 10,
-      responsive: true
-    });
-    therapyTypeFilter.addEventListener('change',  () => therapiesTable.draw());
   }
+
+  // Table setup
+  const tableSelectors = [
+    '#biomarkers-table-result',
+    '#diseases-table-result',
+    '#documents-table-result',
+    '#genes-table-result',
+    '#indications-table-result',
+    '#propositions-therapeutic-response-table-result',
+    '#therapies-table-result'
+  ];
+
+  tableSelectors.forEach(selector => {
+    const el = document.querySelector(selector);
+    if (el) {
+      new DataTable(el, {
+        autoWidth: false,
+        classes: { table: 'table table-striped' },
+        layout: {
+          topStart: 'search',
+          topEnd: 'pageLength',
+          bottomStart: 'info',
+          bottomEnd: 'paging'
+        },
+        pageLength: 10,
+        responsive: true
+      });
+    }
+  });
 });
