ff7bf2dcb6c4d71266a1fe1355e1bc5241420435
Brendan Reardon <9423424+brendanreardon@users.noreply.github.com>
2025-06-05 11:55:34 -0400

    Changed symlink paths

--- switch_instance.sh
+++ switch_instance.sh
@@ -20,7 +20,7 @@ fi
 
 ln -sf "cache-$INSTANCE.sqlite3" data/cache.sqlite3
 ln -sf "deploy/$INSTANCE/config.ini" config.ini
-ln -sf "deploy/$INSTANCE/nginx.conf" service/moalmanac-browser
-ln -sf "deploy/$INSTANCE/secure-application.sh" service/secure-application.sh
+ln -sf "../deploy/$INSTANCE/nginx.conf" service/moalmanac-browser
+ln -sf "../deploy/$INSTANCE/secure-application.sh" service/secure-application.sh
 
 echo "Switched to instance: $INSTANCE"
